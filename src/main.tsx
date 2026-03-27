a04e33b8f9f694e598f664269d2b74ed6bd4996c
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-27 17:18:48 -0300

    Update import comment for App component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Importing the main component
+import App from './App' // Importing the main component App.tsx
 const root = document.getElementById('root');
 
 if (root) {
