eb8b1430dc90d75440582b5e84c572df97d4ab16
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-27 17:11:20 -0300

    Refactor root element selection and import comment

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// Import ensuring that the "A" is capitalized as in your file
-import App from './App'
+import App from './App' // Importing the main component
+const root = document.getElementById('root');
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+if (root) {
+  ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
