499814bf0b077e05f8575089fb5ac37f87d978bf
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-27 17:58:13 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 const root = document.getElementById('root');
-
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
