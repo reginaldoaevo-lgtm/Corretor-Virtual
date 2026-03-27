ad756426a45845ff09b0fbde9f199a8685f5870f
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-27 17:53:36 -0300

    Fix syntax error in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 const root = document.getElementById('root');
+
 if (root) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
