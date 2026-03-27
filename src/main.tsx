558709de67d89224e012b3b0ee7e3e634f565e1e
reginaldoaevo-lgtm <reginaldo.aevo@gmail.com>
2026-03-27 18:31:42 -0300

    Refactor root element rendering in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './index.css' // THIS LINE IS THE KEY TO DESIGN
 import App from './App'
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+const root = document.getElementById('root');
+if (root) {
+  ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <App />
     </React.StrictMode>
